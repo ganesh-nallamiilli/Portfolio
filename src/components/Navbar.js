3e55b07743fc98947efb660f3dab92fe87a53787
GaneshEunimart <umanagaganesh.nallamilli@eunimart.com>
2023-08-23 20:54:21 +0530

    few changes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,36 +1,65 @@
-import React from 'react'
+import React from "react";
 
 function Navbar() {
   return (
-    <section className='m-0 p-0' style={{position:"sticky",top:"0",width:"100%",zIndex:"1"}}>
-        <nav class="navbar navbar-expand-lg navbar-dark" style={{background:"#2C3333"}}>
+    <section
+      className="m-0 p-0 py-2"
+      style={{
+        position: "sticky",
+        top: "0",
+        width: "100%",
+        zIndex: "1",
+        background: "#2C3333",
+      }}
+    >
+      <div className="container">
+        <nav class="navbar navbar-expand-lg navbar-dark">
           <div class="container-fluid">
-            <a class="navbar-brand" href="#about">Ganesh Reddy</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <a class="navbar-brand" href="#about">
+              Ganesh Reddy
+            </a>
+            <button
+              class="navbar-toggler"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#navbarSupportedContent"
+              aria-controls="navbarSupportedContent"
+              aria-expanded="false"
+              aria-label="Toggle navigation"
+            >
               <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
-              <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+              <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
                 <li class="nav-item">
-                  <a class="nav-link " href="#work">Work</a>
+                  <a class="nav-link " href="#work">
+                    Work
+                  </a>
                 </li>
                 <li class="nav-item">
-                  <a class="nav-link" href="#skills">Skills</a>
+                  <a class="nav-link" href="#skills">
+                    Skills
+                  </a>
                 </li>
                 <li class="nav-item">
-                  <a class="nav-link " href="#team">Team</a>
+                  <a class="nav-link " href="#team">
+                    Team
+                  </a>
                 </li>
               </ul>
               <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
-              <li class="nav-item">
-                  <a class="nav-link " href="#hire_me">Hire me</a>
+                <li class="nav-item">
+                  <a class="nav-link " href="#hire_me">
+                    Hire me
+                  </a>
                 </li>
               </ul>
             </div>
           </div>
         </nav>
+      </div>
     </section>
-  )
+  );
 }
 
 export default Navbar;
